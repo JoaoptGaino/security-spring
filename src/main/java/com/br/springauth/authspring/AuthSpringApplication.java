5b9e025ec27efc9aeb7e3ad7c399e504f139162f
JoaoptGaino <joaoptgaino@hotmail.com>
2021-09-06 13:32:27 -0300

    first commit

---
+++ src/main/java/com/br/springauth/authspring/AuthSpringApplication.java
@@ -0,0 +1,11 @@
+package com.br.springauth.authspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/br/springauth/authspring/AuthSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.springauth.authspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
